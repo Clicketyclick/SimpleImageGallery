049b7c3b6151a0111592a4e16e998ea0b71f35e1
Erik Bachmann <Erik@Clicketyclick.dk>
2024-11-18 10:46:02 +0100

    Version and date from Github #10

--- lib/getGitInfo.php
+++ lib/getGitInfo.php
@@ -1,33 +1,35 @@
 <?php
 /**
  *   @file       getGitInfo.php
- *   @brief      $(Brief description)
- *   @details    $(More details)
+ *   @brief      Getting information from `.git`.
+ *   
+ *   @details    
+ *   Function|Brief
+ *   ---|---
+ *   getGitHistory()	| Get history from current `.git`.
+ *   getGitCommitInfo()	| Get latest commit from `.git`.
+ *   getGitVersion()	| Get software version from `.git`.
+ *   getDoxygenHeader()	| Get Doxygen file header expanded for display.
  *   
  *   @copyright  http://www.gnu.org/licenses/lgpl.txt LGPL version 3
  *   @author     Erik Bachmann <ErikBachmann@ClicketyClick.dk>
  *   @since      2024-11-13T10:25:46 / ErBa
- *   @version    2024-11-13T10:36:38
+ *   @version    2024-11-18T09:46:03
  */
 
 //----------------------------------------------------------------------
 
 /**
- *   @fn         getGitHistory
  *   @brief      Get history from current .git
  *   
- *   @param [in]	$branch='main'	Default branch
+ *   @param [in]	$branch	Default branch [main]
  *   @return     array with history
  *   
- *   @details    $(More details)
+ *   @details
+ *   @code
+ *   var_export( getGitHistory() );
+ *   @endcode
  *   
- *   @example    var_export( getGitHistory() );
- *   
- *   @todo       
- *   @bug        
- *   @warning    
- *   
- *   @see        https://
  *   @since      2024-11-13T10:25:52
  */
 function getGitHistory( $branch = 'main' )
@@ -63,15 +65,13 @@ function getGitHistory( $branch = 'main' )
 //----------------------------------------------------------------------
 
 /**
- *   @fn         getGitCommitInfo
  *   @brief      Get latest commit from .git
  *   
- *   @param [in]	VOID
  *   @return     array w. commit info
  *   
  *   @details    
- *   
- *   @example    var_export( getGitCommitInfo() );
+ *   @code
+ *       var_export( getGitCommitInfo() );
  *        array (
  *         0 =>
  *         array (
@@ -82,12 +82,8 @@ function getGitHistory( $branch = 'main' )
  *           'action' => 'clone',
  *           'comment' => 'from https://github.com/....git',
  *         )
- *   
- *   @todo       
- *   @bug        
- *   @warning    
- *   
- *   @see        https://
+ *   @endcode
+ *
  *   @since      2024-11-13T10:26:31
  */
 function getGitCommitInfo()
@@ -168,6 +164,142 @@ function getGitCommitInfo()
 
     return( $commitInfo );
 }	// getGitCommitInfo()
+
+//----------------------------------------------------------------------
+
+/**
+ *   @brief      Get software version from Git
+ *   
+ *   @param [in]	$major=0	Incompatible API changes
+ *   @param [in]	$minor=0	Add functionality in a backward compatible manner
+ *   @param [in]	$patch=1	Backward compatible bug fixes. Overruled by $GitCommitInfo['commitno']
+ *   @param [in]	$level=0	Pre-alpha, Alpha, Beta ...
+ *   @return     String with version
+ *   
+ *   @details
+
+@verbatim
+0.0.265π_2024-11-17T21:05:33_ff52b8d
+│ │ └┬┘│ └──────────────┬──┘ └──┬──┘
+│ │  │ │                │       └─ Hash
+│ │  │ │                └───────── Release date
+│ │  │ └────────────────────────── Level or life cycle
+│ │  └──────────────────────────── Patch or commit no
+│ └─────────────────────────────── Minor
+└───────────────────────────────── Major
+@endverbatim
+ *   
+ *   Life cycle
+ *   
+ *   0. π: Pró or Pre-alpha refers to all activities performed during the software project before formal testing.
+ *   1. α: Alpha software is not thoroughly tested by the developer before it is released to customers.
+ *   2. β: Beta phase generally begins when the software is feature-complete but likely to contain several known or unknown bugs.
+ *   3. γ: Gamma or release candidate (RC) is a beta version with the potential to be a stable product, which is ready to release unless significant bugs emerge.
+ *   4. ω: Omega or stable release is the last release candidate (RC) which has passed all stages of verification and tests. Any known remaining bugs are considered acceptable. This release goes to production.
+ *   5. τ: Tau or EOL When software is no longer sold or supported, the product is said to have reached end-of-life, to be discontinued, retired, deprecated, abandoned, or obsolete
+ *   
+ *   @code
+ *   echo getGitVersion();
+ *   @endcode
+@verbatim
+0.0.265π_2024-11-17T21:05:33_ff52b8d
+@endverbatim
+ *   
+ *@code
+ *   echo getGitVersion( );
+ *@endcode
+ * Will either produce a version from `./version.txt` appended Git commit no
+@verbatim
+v.0.0.1-alpha_265
+@endverbatim
+ * OR version no based on arguments + data from `getGitCommitInfo()`
+@verbatim
+v.0.0.265-π_2024-11-17T21:05:33_ff52b8d
+@endverbatim
+ *
+ *@code
+ *   echo getGitVersion( 1, 2, 3, 0);
+ *@endcode
+@verbatim
+1.2.3π_2024-11-17T21:05:33_ff52b8d
+@endverbatim
+ *
+ * @note Patch will be overruled by `$GitCommitInfo['commitno']`
+ *
+ *   @since      2024-11-18T05:11:25
+ */
+function getGitVersion($major = 0, $minor = 0, $patch = 1, $level = 0)
+{
+	$lifecycle	= [
+		'π'	// Pró or Pre-alpha refers to all activities performed during the software project before formal testing.
+	,	'α'	// Alpha software is not thoroughly tested by the developer before it is released to customers.
+	,	'β'	// Beta phase generally begins when the software is feature-complete but likely to contain several known or unknown bugs.
+	,	'γ'	// Gamma or release candidate (RC) is a beta version with the potential to be a stable product, which is ready to release unless significant bugs emerge.
+	,	'ω'	// Omega or stable release is the last release candidate (RC) which has passed all stages of verification and tests. Any known remaining bugs are considered acceptable. This release goes to production.
+	,	'τ'	// Tau or EOL When software is no longer sold or supported, the product is said to have reached end-of-life, to be discontinued, retired, deprecated, abandoned, or obsolete
+	];
+
+	$GitCommitInfo	= getGitCommitInfo();
+	
+	// Get version from `version.txt`: v.0.0.1-alpha
+	$gitVersion		= trim( @file_get_contents( './version.txt' ) );
+	
+	if ( empty( $gitVersion ) )	// Else build
+	{
+		$gitVersion	= sprintf( "v.%s.%s.%s-%s_%s_%s"
+		,	$major
+		,	$minor
+		,	$GitCommitInfo['commitno'] ?? $patch
+		,	$lifecycle[ $level ?? 0 ]
+		,	str_replace( ' ', 'T', $GitCommitInfo['commitdate'] )
+		,	$GitCommitInfo['shorthash']
+		//,	$GitCommitInfo[]
+		);
+	}
+	else
+	{
+		$gitVersion .= '_' . $GitCommitInfo['commitno'] ?? $patch;
+	}
+	return( $gitVersion );
+}	// getGitVersion()
+
+//----------------------------------------------------------------------
+
+/**
+ *   @brief      Get Doxygen file header expanded for display.
+ *   
+ *   @return     String w. expanded Doxygen header.
+ *   
+ *   @details    Will expand '@include version.txt' to content of file
+ *   Default is build from `.git`:
+@verbatim
+\@version    v.0.0.265-π_2024-11-17T21:05:33_ff52b8d
+@endverbatim
+ *   But if  `version.txt` exists this will be include appended with file date:
+@verbatim
+\@version    v.0.0.1-alpha 2024-11-13T07:26:33+00:00
+@endverbatim
+ *
+ *   @note       '@include version.txt' is implemented.
+ *   
+ *   @since      2024-11-18T09:42:02
+ */
+function getDoxygenHeader( $file )
+{
+	preg_match('/\/\*\*(.*?)\*\//s', implode( '', file( $file )), $match); 
+	return( 
+		str_replace( 
+			'@include version.txt'
+		,	file_exists('./version.txt') 
+			?	trim(file_get_contents('./version.txt') )
+				.	' '
+				.	date('c', filectime( $file ))
+			:	getGitVersion() 
+		,	$match[1]
+		)
+	.	PHP_EOL 
+	);
+}	// getDoxygenHeader()
 
 //----------------------------------------------------------------------
 
--- lib/getGitInfo_test.php
+++ lib/getGitInfo_test.php
@@ -1,8 +1,10 @@
 <?php
 /**
  *   @file       getGitInfo_test.php
- *   @brief      $(Brief description)
- *   @details    $(More details)
+ *   @brief      Testing getGitInfo
+ *   @details    
+ * @var  mixed $GitCommitInfo
+ * Description
  *   
  *   @copyright  http://www.gnu.org/licenses/lgpl.txt LGPL version 3
  *   @author     Erik Bachmann <ErikBachmann@ClicketyClick.dk>
@@ -12,7 +14,34 @@
 
 include_once( __DIR__. '/getGitInfo.php');
 
-var_export( getGitCommitInfo() );
+echo PHP_EOL . 'getGitHistory: ';
 var_export( getGitHistory() );
+
+echo PHP_EOL . 'getGitCommitInfo: ';
+$GitCommitInfo	= getGitCommitInfo();
+var_export( getGitCommitInfo() );
+
+echo PHP_EOL.'getGitVersion: ';
+var_export( getGitVersion() );
+
+
+echo PHP_EOL.'getGitVersion( 1, 2, 3, 0): ';
+var_export( getGitVersion( 1, 2, 3, 0) );
+
+;
+/*
+array (
+  'gitdir' => '.git',
+  'refPath' => '.git/refs/heads/main',
+  'branch' => 'main',
+  'shorthash' => 'ff52b8d',
+  'hash' => 'ff52b8def54bd709d8d4fa21da461a8e1ceaa9a8',
+  'commitdate' => '2024-11-17 21:05:33',
+  'committer' => 'Erik Bachmann <Erik@Clicketyclick.dk>',
+  'author' => 'Erik Bachmann <Erik@Clicketyclick.dk>',
+  'commitno' => '265',
+*/
+
+
 
 ?>
